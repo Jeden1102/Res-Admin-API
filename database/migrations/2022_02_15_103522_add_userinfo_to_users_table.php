a33f6a934cb153142ad3075794c0f746bdabc372
Jeden1102 <bum123@o2.pl>
2022-02-15 12:10:05 +0100

    initalaze

---
+++ database/migrations/2022_02_15_103522_add_userinfo_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserinfoToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('surname');
+            $table->double('salary');
+            $table->boolean('isAdmin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
