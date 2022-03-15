ebfa4e9b2527bff78a35f0cad19e3e216fdd27f8
Jeden1102 <bum123@o2.pl>
2022-03-15 19:34:45 +0100

    hotfix

--- database/migrations/2022_03_05_165355_add_columns_to_users_table.php
+++ database/migrations/2022_03_05_165355_add_columns_to_users_table.php
@@ -14,7 +14,7 @@ class AddColumnsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('photo_url');
+            $table->string('photo_url')->nullable();
         });
     }
 
