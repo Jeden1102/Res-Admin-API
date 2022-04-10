52774f98a5e62bbad8f3d41b8c918df5eb130cc4
Jeden1102 <bum123@o2.pl>
2022-04-10 14:24:12 +0200

    hotfix mail

--- resources/views/emails/orderManage.blade.php
+++ resources/views/emails/orderManage.blade.php
@@ -1,12 +1,28 @@
 @component('mail::message')
-# Introduction
-{{ $info['email'] }}
-Your order has been confirmed :)))
+# Thank you {{ $info->name }}
 
-@component('mail::button', ['url' => ''])
-Button Text
+Your order has succesfully confirmed !:)
+<h2 style="color:#F47321">Ordered products:</h2>
+<ul>
+@foreach (json_decode($info['items']) as $item)
+    <li> {{ $item->name }} -  {{ $item->size }} - ${{ $item->price }}</li>
+@endforeach
+</ul>
+<hr/>
+<h2 style="color:#F47321">Summary:</h2>
+<p>Summ price : ${{ $info['sumPrice']}}</p>
+<hr/>
+<h2 style="color:#F47321">Delivery:</h2>
+<p>{{ $info['date'] }}, {{ $info['time'] }} </p>
+<p>{{ $info['city'] }}, {{ $info['street'] }} {{ $info['number'] }}  </p>
+@component('mail::button', ['url' => 'https://dominikraducki.works/FoodPenguin/'])
+Food Penguin
 @endcomponent
-
-Thanks,<br>
-{{ config('app.name') }}
+<p>If there are any issues with the order please contact us (+48) 77 461 66 12 with the order number : {{ $info['id'] }}</p>
+<hr/>
+<p>
+    Please prepare the calculated money.
+</p>
+Thanks for your patience,<br>
+Food Penugin Team :)
 @endcomponent
